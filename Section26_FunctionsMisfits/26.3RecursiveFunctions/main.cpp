6629f8ca858662f251cdd26c3388e9b7596ab107
JallaJenkins <68257894+JallaJenkins@users.noreply.github.com>
2023-01-31 10:30:33 -0800

    Section 26 complete

--- Section26_FunctionsMisfits/26.3RecursiveFunctions/main.cpp
+++ Section26_FunctionsMisfits/26.3RecursiveFunctions/main.cpp
@@ -7,6 +7,11 @@ size_t sum_up_to_zero(size_t value) {
   return 0;
 }
 
+unsigned int factorial(unsigned int n) {
+  if (n <= 1) return 1;
+  return n * factorial(n-1);
+}
+
 int main(){
 
   std::cout << "result: " << sum_up_to_zero(10) << '\n';
