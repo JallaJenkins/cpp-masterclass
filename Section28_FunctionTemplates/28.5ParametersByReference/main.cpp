8958267bf581f17dc1fa9f1b9de25c9bc0b85d60
JallaJenkins <wakeupalready@gmail.com>
2023-02-10 17:58:28 -0800

    28.5ParametersByReference

--- Section28_FunctionTemplates/28.5ParametersByReference/main.cpp
+++ Section28_FunctionTemplates/28.5ParametersByReference/main.cpp
@@ -1,8 +1,27 @@
 #include <iostream>
 
+template <typename T> const T& maximum(const T& a, const T& b);
+// template <typename T> T maximum(T a, T b);
+
 int main(){
 
-  
+  double a{23.5};
+  double b{51.2};
+
+  std::cout << "Out - $a: " << &a << '\n';
+  auto result = maximum(a, b);
+  std::cout << "Out - $a: " << &a << '\n';
 
   return 0;
 }
+
+template <typename T> const T& maximum(const T& a, const T& b) {
+  // ++a; // compiler error
+  std::cout << "In - $a: " << &a << '\n';
+  return (a > b) ? a : b;
+}
+
+// template <typename T> T maximum(T a, T b) {
+//   std::cout << "In - $a: " << &a << '\n';
+//   return (a > b) ? a : b;
+// }
