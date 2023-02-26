9c37343fd7493b0451a48e920dffd98b017933dd
JallaJenkins <68257894+JallaJenkins@users.noreply.github.com>
2023-02-26 10:20:30 -0800

    28.25ConstexprIf

---
+++ Section28_FunctionTemplates/28.25ConstexprIf/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include<type_traits>
+
+
+
+void func_floating_point(double d){
+  std::cout << "floating point func...\n";
+}
+
+void func_integral(int i){
+  std::cout << "integral algo...\n";
+}
+
+template <typename T>
+void func(T t)
+{
+  if constexpr(std::is_integral_v<T>)
+    func_integral(t);
+  else if constexpr(std::is_floating_point_v<T>)
+    func_floating_point(t);
+  else
+    static_assert(std::is_integral_v<T> || std::is_floating_point_v<T>,
+      "Argument must be integral or floating point");
+}
+
+int main(){
+
+  func(12);
+
+  return 0;
+}
