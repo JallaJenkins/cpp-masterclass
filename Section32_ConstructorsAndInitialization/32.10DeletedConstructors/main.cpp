23f26f198a0c3ed4880e5c89d8ddf11b20dbbf8d
JallaJenkins <68257894+JallaJenkins@users.noreply.github.com>
2023-04-01 16:55:57 -0700

    32.10DeletedConstructors

---
+++ Section32_ConstructorsAndInitialization/32.10DeletedConstructors/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include "point.h"
+
+int main(){
+
+  // Point p1(21.6, 78.3);
+  // Point p1; // Compiler error - constructor has been deleted
+
+  // Point p0(4.6, 5.2);
+  // Point p1(p0); // Compiler error - copy constructor has been deleted
+
+  // Point p1(std::move(Point(4.6, 5.2)));  // Compiler error - move constructor has been deleted
+
+  // p1.print_info();
+
+
+
+  return 0;
+}
