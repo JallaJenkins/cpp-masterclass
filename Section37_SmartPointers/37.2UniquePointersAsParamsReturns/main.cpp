406437849db3e9a68ea09e9802b5840d2a807727
JallaJenkins <68257894+JallaJenkins@users.noreply.github.com>
2023-06-18 14:30:17 -0700

    37.2 partial

---
+++ Section37_SmartPointers/37.2UniquePointersAsParamsReturns/main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <memory>
+#include "dog.h"
+#include "person.h"
+
+void do_something_with_dog_v1(std::unique_ptr<Dog> d){
+  d->print_info();
+}
+
+int main(){
+
+  /*
+  // Passing unique ptr to function by value
+  std::unique_ptr<Dog> p_dog_1 = std::make_unique<Dog>("Dog1");
+
+  // Can't pass unique_ptr by value to a function b/c copies not allowed
+  // do_something_with_dog_v1(p_dog_1);  // Compiler error
+
+  // Use std::move
+  // Owndership of the pointer will move to the function body
+  // p_dog_1 is reset to nullptr
+  do_something_with_dog_v1(std::move(p_dog_1));
+
+  std::cout << "p_dog_1: " << p_dog_1.get() << '\n';
+  std::cout << "Back to the main function\n";\
+  */
+
+  std::cout << "---------\n";
+
+  std::unique_ptr<Dog> p_dog_1 = std::make_unique<Dog>("Dog1");
+
+  Person person1("John");
+  person1.adopt_dog(std::move(p_dog_1)); // Move to fn and reset to nullptr
+  std::cout << "Doing something, p_dog_1 poitns to: " << p_dog_1.get() << '\n';
+
+  return 0;
+}
