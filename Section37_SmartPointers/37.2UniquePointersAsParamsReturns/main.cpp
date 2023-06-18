178d5a060092de990ae97246d01f67204acada8f
JallaJenkins <68257894+JallaJenkins@users.noreply.github.com>
2023-06-18 14:53:23 -0700

    37.2 Partial 2

--- Section37_SmartPointers/37.2UniquePointersAsParamsReturns/main.cpp
+++ Section37_SmartPointers/37.2UniquePointersAsParamsReturns/main.cpp
@@ -7,6 +7,12 @@ void do_something_with_dog_v1(std::unique_ptr<Dog> d){
   d->print_info();
 }
 
+void do_something_with_dog_v2(const std::unique_ptr<Dog> &d){
+  d->set_dog_name("Rior");
+  d->print_info();
+  // d.reset(); // Compiler error
+}
+
 int main(){
 
   /*
@@ -27,11 +33,18 @@ int main(){
 
   std::cout << "---------\n";
 
-  std::unique_ptr<Dog> p_dog_1 = std::make_unique<Dog>("Dog1");
+  // std::unique_ptr<Dog> p_dog_1 = std::make_unique<Dog>("Dog1");
 
+  /*
   Person person1("John");
   person1.adopt_dog(std::move(p_dog_1)); // Move to fn and reset to nullptr
   std::cout << "Doing something, p_dog_1 poitns to: " << p_dog_1.get() << '\n';
+  */
+
+  // An implicit move occurs when temp object is created in place
+  // Avoids error of sending existing dog pointer to function
+  do_something_with_dog_v1(std::make_unique<Dog>("Temporary Dog"));
+  std::cout << "delimiter\n";
 
   return 0;
 }
