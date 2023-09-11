7ba436c86d66b755561d563e201bfa7793872be8
JallaJenkins <wakeupalready@gmail.com>
2023-09-11 15:25:43 -0700

    41.14PolyFunctionsAccessSpecifiers

---
+++ Section41_Polymorphism/41.13VirtualOverrideNotKeywords/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+// These will work as class names - but don't do it!
+class override{
+
+};
+
+// class if{  // Compiler error - "if" is a keyword
+
+// };
+
+
+int main(){
+
+  // These will work as variables - but don't do it!
+  // double final{3.4};
+  // std::cout << "final: " << final << '\n';
+
+  // double override{3.4};
+  // std::cout << "override: " << override << '\n';
+
+  return 0;
+}
