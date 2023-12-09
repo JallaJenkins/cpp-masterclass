7ce110545d84fa4af1ef67e424bc3af4c090315f
JallaJenkins <wakeupalready@gmail.com>
2023-12-09 13:51:08 -0800

    44.13StreamInertionOperator

--- Section44_ClassTemplates/44.12FriendFunctions/main.cpp
+++ Section44_ClassTemplates/44.12FriendFunctions/main.cpp
@@ -1,12 +1,47 @@
 #include <iostream>
 
-template<typename T>
-class TemplateClass;  // forward decl to make func decl possible
+// template<typename T>
+// class TemplateClass;  // class template forward decl to make func decl possible
+
+// template<typename T>
+// void some_func(TemplateClass<T> param); // function template declaration
 
 template<typename T>
-void some_func(TemplateClass<T>);
+class TemplateClass{
+  friend void some_func<T>(TemplateClass<T> param);
+
+public:
+  explicit TemplateClass<T>(){
+
+  }
+  void set_up(T param){
+    m_var = param;
+  }
+  void do_something(const T a, T b) {
+    std::cout << "Doing something with " << a << " and " << b << '\n';
+  }
+
+private:
+  T m_var;
+};
+
+template <typename T>
+void some_func(TemplateClass<T> param){
+  std::cout << "Inside some_func, accessing private data of TemplateClass: "
+            << param.m_var << '\n';
+}
+
 
 int main(){
+
+  TemplateClass<int> object1;
+  object1.set_up(10);
+
+  TemplateClass<double> object2;
+  object2.set_up(12.2);
+
+  some_func(object1);
+  some_func(object2);
 
   return 0;
 }
---
+++ Section44_ClassTemplates/44.13StreamInsertionOperator/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "boxcontainer.h"
+
+int main(){
+
+  BoxContainer<int> int_box;
+	int_box.add(1);
+	int_box.add(2);
+	int_box.add(10);
+	int_box.add(15);
+	std::cout << "int_box: " << int_box << '\n';
+
+  return 0;
+}
