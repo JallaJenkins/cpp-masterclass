ae90f97d4cad0f8292cb943be61bbcea73d7bccf
JallaJenkins <wakeupalready@gmail.com>
2024-01-14 16:07:38 -0800

    47.8Exercise105

--- Section47_STLContainersIterators/47.8Exercise105/main.cpp
+++ Section47_STLContainersIterators/47.8Exercise105/main.cpp
@@ -118,39 +118,34 @@ std::list<T> union_func(const std::list<T>& l1, const std::list<T>& l2){
   
   */
 
-  while( (itl1!=l1.end()) &&(itl2!=l2.end())){
+  while(itl1 != l1.end() && itl2 != l2.end()){
 
-      if(*itl1 == *itl2){
-            result.push_back(*itl1);
-            ++itl1;
-            ++itl2;
-        }else if( *itl1 < *itl2){
-            result.push_back(*itl1);
-            ++itl1;
-        }else{
-            result.push_back(*itl2);
-            ++itl2;
-        }
+    if(*itl1 < *itl2){
+      result.push_back(*itl1);
+      ++ itl1;
+    } else if (*itl1 > *itl2) {
+      result.push_back(*itl2);
+      ++itl2;
+    } else {
+      ++itl1;
     }
 
-    //Handle remaining elements from bigger list
-    while(itl1!=l1.end()){
-        result.push_back(*itl1);
-        ++itl1;
-    }
+  }
 
-    while(itl2!=l2.end()){
-        result.push_back(*itl2);
-        ++itl2;
-    }
+  while(itl1 != l1.end()){
+    if(*itl1 > result.back())
+    result.push_back(*itl1);
+    ++itl1;
+  }
 
-  //YOUR CODE WILL GO ABOVE THIS LINE 
-  //DON'T MODIFY ANYTHING BELOW THIS LINE
+  while(itl2 != l2.end()){
+    if(*itl2 > result.back())
+    result.push_back(*itl2);
+    ++itl2;
+  }
 
   return result;
-
 }
-
 
 template <typename T>
 void print_list( const std::list<T>& l){
@@ -165,8 +160,8 @@ void print_list( const std::list<T>& l){
 
 int main(){
 
-  std::list<int> l1 = {3, 4, 5,7,11};
-  std::list<int> l2 = {1, 2, 3, 4, 6, 7, 9, 12, 27};
+  std::list<int> l1 = {1, 1, 3, 4, 5, 7, 11, 27, 27, 27};
+  std::list<int> l2 = {1, 1, 2, 3, 4, 6, 27, 27, 27};
 
   print_list(l1);
   print_list(l2);
