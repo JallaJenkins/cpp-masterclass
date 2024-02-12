82849eb175ea278c5d2ea3d0493a588f0a16b380
JallaJenkins <wakeupalready@gmail.com>
2024-02-12 08:24:56 -0800

    52.1Co_await

--- Section52_Coroutines/52.1Co_await/main.cpp
+++ Section52_Coroutines/52.1Co_await/main.cpp
@@ -24,7 +24,40 @@ struct CoroType {
   std::coroutine_handle<promise_type>   m_handle;
 };
 
+CoroType do_work() {
+  std::cout << "Doing first thing...\n";
+  co_await std::suspend_always();
+  std::cout << "Doing second thing...\n";
+  co_await std::suspend_always();
+  std::cout << "Doing third thing...\n";
+}
+
 int main(){
 
+  // Init coroutine and initial suspend
+  // Note if we set initial suspend to suspend_never in the class then the init will run the coroutine until the first explicit suspent
+  auto task = do_work();
+  // auto task2 = do_work();
+
+  // Resume
+  task.m_handle();
+  // task.m_handle();
+  std::cout << std::boolalpha;
+  std::cout << "coro done: " << task.m_handle.done() << '\n';
+
+  // Resume
+  task.m_handle.resume();
+  std::cout << "coro done: " << task.m_handle.done() << '\n';
+
+  // Resume
+  task.m_handle.resume();
+  std::cout << "coro done: " << task.m_handle.done() << '\n';
+
+  std::cout << "------------------------\n";
+
+  // Resume after completion REALLY BAD
+  // task.m_handle.resume();   // Crash!
+
+  std::cout << "Done!\n";
   return 0;
 }
